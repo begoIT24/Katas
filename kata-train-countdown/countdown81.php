9e84b961dc374be7e0d6d1f66bccb36bfe8123df
begoIT24 <blopezprof@gmail.com>
2024-05-06 08:54:45 +0200

    the 2 parts finished

--- kata-train-countdown/countdown81.php
+++ kata-train-countdown/countdown81.php
@@ -1,13 +1,14 @@
 <?php
+
 echo "Per salvar la vida has d'intentar frenar aquest tren introduint 10 números! \n";
 for ($i = 0; $i < 10; $i++) {
-    echo "Introdueix un número: " ;
-    $num = (int)readline();
-    $numeros[] = $num;          //mejor que array_push() porque así no llamas a una función
+    echo "Introdueix un número:  " ;
+    $num = (int)readline();  
+    $numeros[] = $num;
 }
 $sumaTotal = array_sum($numeros);
 
-if ($sumaTotal == 67) {
+if ($sumaTotal == 81) {
     echo "Has aconseguit activar el fre d'emergència. T'has salvat! \n";
 } else {
     echo "No has aconseguit frenar el tren \n";
