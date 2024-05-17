5f4aa9c2c28cf5da7bb4ea45cb86b231598763bb
begoIT24 <blopezprof@gmail.com>
2024-05-17 10:11:07 +0200

    class Player finished

---
+++ kata_dungeons&dragons/Player.php
@@ -0,0 +1,52 @@
+<?php
+
+class Player
+{
+   protected string $name;
+   protected int $posX;
+   protected int $posY;
+   protected const MAX_POS = 9;
+   protected const MIN_POS = 0;
+
+   public function __construct($name, $posX, $posY)
+   {
+      $this->name = $name;
+      $this->posX = $posX;
+      $this->posY = $posY;
+   }
+
+   public function move($direction)
+   {
+      switch ($direction) {
+         case 'up':
+            if ($this->posY < self::MAX_POS) {
+               $this->posY++;
+            } else {
+               return "You can't move up";
+            }
+            break;
+         case 'down':
+            if ($this->posY > self::MIN_POS) {
+               $this->posY--;
+            } else {
+               return "You can't move down";
+            }
+            break;
+         case 'left':
+            if ($this->posX > self::MIN_POS) {
+               $this->posX--;
+            } else {
+               return "You can't move to the left";
+            }
+            break;
+         case 'right':
+            if ($this->posX < self::MAX_POS) {
+               $this->posX++;
+            } else {
+               return "You can't move to the right";
+            }
+            break;
+      }
+      return "You have moved to the position ($this->posX, $this->posY)";
+   }
+}
